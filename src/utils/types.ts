8e1e29e8ede47b3a7e53bbf1bbe1a83bb4e77ec8
Harry Borrelli <hborrelli@outlook.com>
2024-05-08 13:09:05 -0600

    Add types.

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,3 +2,16 @@ export type TPokemon = {
   name: string;
   url: string;
 }
+
+export type TSearchData = {
+  'page': string | string[];
+  'perPage': string | number | string[];
+  'start': number;
+  'end': number;
+  'totalPages': number;
+}
+
+export type TResults = {
+  name: string;
+  url: string;
+}[]
